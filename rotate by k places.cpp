84d8a5033a097b43d300675983bc437028d36540
Ambe03 <ambika032003@gmail.com>
2025-01-27 16:33:05 +0545

    optimal approach

--- rotate by k places.cpp
+++ rotate by k places.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void rotate(int arr[], int n,int k)
 {
+	k = k%n;
 	int* temp = new int[k]; 
 	for(int i=0; i<k; i++)
 	{
@@ -27,7 +28,28 @@ void rotate(int arr[], int n,int k)
 	
 }
 
+//OPTIMAL APPROACH ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+void reverse(int arr[], int start ,int end)
+{
+	while(start<=end)
+	{
+		int temp = arr[start];
+	    arr[start] = arr[end];
+		arr[end]= temp;
+		start ++;
+		end--;
+	}
+}
+
+void rotate2(int arr[], int n, int k)
+{
+//	int n = arr.size();
+	reverse(arr,0, n-k-1);
+	reverse(arr, n-k, n-1);
+	reverse(arr, 0, n-1);
+	
+}
 
 int main()
 {
@@ -42,7 +64,7 @@ int main()
 		cout<<arr[i]<<" ";
 	}
 	
-	rotate(arr, n, k);
+	rotate2(arr,n, k);
 	cout<<"ROTATED ARRAY: ";
 	for(int i=0; i<n;i++)
 	{
